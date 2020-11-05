6302d7bdb82eea01d758ca174e5afd98d5aa295a
rafal-strozyk <rafalstrozyk@onet.pl>
2020-11-05 15:07:34 +0100

    scroll smooth

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-setInterval(()=>{
+window.addEventListener('DOMContentLoaded',setInterval(()=>{
     let today = new Date();
     let day = today.getDate();
-    let month = today.getMonth();
+    let month = today.getMonth()+1;
     let year = today.getFullYear();
     let hours = today.getHours() < 10 ? "0"+today.getHours() : today.getHours();
     let minutes = today.getMinutes() < 10 ? "0"+today.getMinutes() : today.getMinutes();
     let seconds = today.getSeconds() < 10 ? "0"+today.getSeconds() : today.getSeconds();
     document.getElementById("data").innerHTML = day+'.'+month+'.'+year+' '+hours+':'+minutes+':'+seconds;
-},1000);
-
+},1000));
