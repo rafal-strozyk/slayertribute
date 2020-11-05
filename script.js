4ab11b4d06e72b4e7f931bfbc455862c17060e20
rafal-strozyk <rafalstrozyk@onet.pl>
2020-11-05 12:55:21 +0100

    corrected clock

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 setInterval(()=>{
-    var today = new Date();
-    if(today.getSeconds()<10){
-        document.getElementById("data").innerHTML= today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear()+' '+today.getHours()+':'+today.getMinutes()+':'+0+today.getSeconds();
-    }
-    else{
-        document.getElementById("data").innerHTML= today.getDate()+'.'+(today.getMonth()+1)+'.'+today.getFullYear()+' '+today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
-    }
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let hours = today.getHours() < 10 ? "0"+today.getHours() : today.getHours();
+    let minutes = today.getMinutes() < 10 ? "0"+today.getMinutes() : today.getMinutes();
+    let seconds = today.getSeconds() < 10 ? "0"+today.getSeconds() : today.getSeconds();
+    document.getElementById("data").innerHTML = day+'.'+month+'.'+year+' '+hours+':'+minutes+':'+seconds;
 },1000);
 
