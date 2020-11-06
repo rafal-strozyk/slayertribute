cf34f12f79f720665f2a7dbd7570ce0a9e0ed864
rafal-strozyk <rafalstrozyk@onet.pl>
2020-11-06 19:37:02 +0100

    final

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-window.addEventListener('DOMContentLoaded',setInterval(()=>{
-    let today = new Date();
-    let day = today.getDate();
-    let month = today.getMonth()+1;
-    let year = today.getFullYear();
-    let hours = today.getHours() < 10 ? "0"+today.getHours() : today.getHours();
-    let minutes = today.getMinutes() < 10 ? "0"+today.getMinutes() : today.getMinutes();
-    let seconds = today.getSeconds() < 10 ? "0"+today.getSeconds() : today.getSeconds();
-    document.getElementById("data").innerHTML = day+'.'+month+'.'+year+' '+hours+':'+minutes+':'+seconds;
-},1000));
+window.addEventListener('DOMContentLoaded', () => {
+    function updateDate() {
+        let today = new Date();
+        let day = today.getDate();
+        let month = today.getMonth() + 1;
+        let year = today.getFullYear();
+        let hours = today.getHours() < 10 ? "0" + today.getHours() : today.getHours();
+        let minutes = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
+        let seconds = today.getSeconds() < 10 ? "0" + today.getSeconds() : today.getSeconds();
+        document.getElementById("data").innerHTML = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes + ':' + seconds;
+    }
+    updateDate();
+    setInterval(updateDate, 1000);
+});
